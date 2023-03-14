7ab678bffda73771a567336af1314060cd48388a
Roman <mail@romanpeters.nl>
2023-03-14 23:26:08 +0100

    Update pcconfig.py

--- pcconfig.py
+++ pcconfig.py
@@ -3,6 +3,5 @@ import pynecone as pc
 config = pc.Config(
     app_name="whisper_ui",
     db_url="sqlite:///pynecone.db",
-    api_url="http://10.10.20.20:8000",
-#    env=pc.Env.DEV,
+    api_url="http://0.0.0.0:8000",
 )
